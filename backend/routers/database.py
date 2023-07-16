47d4b3f1b8fdb11ea9fd1429de4f44fbfb0490cb
hykhhijk <hjhhijk@gmail.com>
2023-07-16 16:09:08 +0000

    [Fix] fix db port&delete user_id

--- backend/routers/database.py
+++ backend/routers/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql://root:1234@localhost:30007/user_db?charset=utf8"        #change user root, password 1234 for right setting
+SQLALCHEMY_DATABASE_URL = "mysql://root:1234@localhost:3306/user_db?charset=utf8"        #change user root, password 1234 for right setting
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)     #db session
--- backend/routers/model.py
+++ backend/routers/model.py
@@ -7,7 +7,7 @@ from backend.routers.database import Base
 class User(Base):
     __tablename__ = "users"
 
-    user_id = Column(Integer, primary_key=True)
-    username = Column(String, index=True)      #식별자라는데 정확히는 모르겠다
+    # user_id = Column(Integer, primary_key=True)
+    username = Column(String, index=True, primary_key=True)      #식별자라는데 정확히는 모르겠다
     hashed_password = Column(String, unique=True, index=True)
     signin_name = Column(String)
